5096049101950abd1618e0c88885f0b7e3d8cada
Leon Styven <j.stca03@gmail.com>
2025-10-09 18:37:58 -0500

    Update ProductDescription component styling: enhance text colors, spacing, and font sizes for improved readability and visual appeal.

--- Frontend/web/src/components/shared/atoms/productDescription.tsx
+++ Frontend/web/src/components/shared/atoms/productDescription.tsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const ProductDescription : React.FC = () => {
     return (
-        <div className="flex flex-col">
-            <span className="font-bold font-sx text-slate-500">Brand</span>
-            <h3 className="font-regular font-medium">Title</h3>
-            <div className="flex flex-row">
-                <span className="line-through">$84.990</span>
+        <div className="flex flex-col text-black">
+            <span className="font-bold font-sx text-gray-500">Brand</span>
+            <h3 className="font-lg font-medium mt-4">Title</h3>
+            <div className="flex flex-row gap-2 font-bold">
+                <span className="line-through text-gray-400">$84.990</span>
                 <span>$59.493</span>
             </div>
-        </div>
+        </div>  
 
     );
 };
