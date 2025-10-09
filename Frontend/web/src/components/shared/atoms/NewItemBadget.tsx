9438c3fb001bef163a3450e18b0ad6aa565af4a5
Leon Styven <j.stca03@gmail.com>
2025-10-09 17:29:31 -0500

    Add NewItemBadget and PromotionBadget components: create reusable badges for new items and promotions with Tailwind CSS styling.

---
+++ Frontend/web/src/components/shared/atoms/NewItemBadget.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NewItemBadget: React.FC = () => {
+    return (
+        <div className="bg-slate-800 text-white font-bold text-xs w-fit py-1 px-2">NEW</div>
+    );
+};
+
+export default NewItemBadget
---
+++ Frontend/web/src/components/shared/atoms/PromotionBadget.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PromotionBadget: React.FC<{ amount: number }> = (props) => {
+    return (
+        <div className="bg-red-500 text-white font-bold text-xs w-fit py-1 px-2">{`-${props.amount}%`}</div>
+    );
+};
+
+export default PromotionBadget
