05ce7c9d387b90cfcf085639c4c99f378eaa9433
Leon Styven <j.stca03@gmail.com>
2025-10-10 13:15:15 -0500

    Add ProductCountStepper component and integrate into SideNavItem: enhance product quantity selection functionality. Update index.html to include Material Symbols font for improved iconography.

---
+++ Frontend/web/src/components/shared/atoms/productCountStepper.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const ProductCountStepper : React.FC<{minValue: number, maxValue: number}> = ({minValue = 1, maxValue = 5}) => {
+
+    const [count, setCount] = useState(minValue);
+
+    const handleIncrementCounter = () => {
+        if(count < maxValue) {
+            setCount ((prevState) => prevState + 1);
+        }
+    };
+
+    const handleDecrementCounter = () => {
+        if(count > minValue) {
+            setCount ((prevState) => prevState - 1);
+        }
+    };
+
+
+    return(
+        <div style={{
+            border: '1px, solid #e3e4e6',
+            transition: 'border 0.3s ease'
+        }} className="btn-group flex rounded-lg w-fit align-middle overflow-hidden">
+            
+            <button className="decrement-btn py-2 px-4 cursor-pointer border-r-1 border-[#e3e4e6]" onClick={handleDecrementCounter}>
+                <span className="">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#727273"><path d="M240-440q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h480q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H240Z"/></svg>
+                </span>
+            </button>
+
+            <p className="inline-block mx-6 content-center">{count}</p>
+
+            
+
+            <button className="increment-btn py-2 px-4 cursor-pointer border-l-1 border-[#e3e4e6]" onClick={handleIncrementCounter}>
+                <span className="">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#727273"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg>
+                </span>
+            </button>
+        </div>
+    );
+};
+
+export default ProductCountStepper
--- Frontend/web/src/components/shared/organisms/sideNavCart.tsx
+++ Frontend/web/src/components/shared/organisms/sideNavCart.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SideNavTitle from "../molecules/sideNavTitle";
+import SideNavItem from "../molecules/sideNavItem";
 
 const SideNavCart : React.FC = () => {
     return (
         <div className="container w-full h-full">
             <SideNavTitle/>
+            <SideNavItem/>
         </div>
     );
 };
