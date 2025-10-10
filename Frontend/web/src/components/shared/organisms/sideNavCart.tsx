0688315d45436705bf064d9f5a524bdcc94a4ee3
Leon Styven <j.stca03@gmail.com>
2025-10-10 14:28:52 -0500

    Refactor SideNav components: replace SideNavItem with SideNavItems to enhance product display and integrate ProductCountStepper for quantity selection. Remove unused sideNavItem component and update styles for improved layout.

--- Frontend/web/src/components/shared/organisms/sideNavCart.tsx
+++ Frontend/web/src/components/shared/organisms/sideNavCart.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import SideNavTitle from "../molecules/sideNavTitle";
-import SideNavItem from "../molecules/sideNavItem";
+import SideNavItems from "../molecules/sideNavItems";
 
 const SideNavCart : React.FC = () => {
     return (
         <div className="container w-full h-full">
             <SideNavTitle/>
-            <SideNavItem/>
+            <SideNavItems/>
         </div>
     );
 };
